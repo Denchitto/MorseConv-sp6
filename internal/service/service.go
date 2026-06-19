c914eb1dbdf8e26c81e63c83a50e66e6088e2042
Matveev Denis <denchittos@gmail.com>
2026-06-19 07:15:29 +0300

    First commit

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"MorseConv-sp6/pkg/morse"
+	"strings"
+)
+
+func MorseConv(data string) string {
+
+	runesRemover := func(r rune) rune {
+		switch r {
+		case ' ', '-', '.':
+			return -1
+		}
+		return r
+	}
+	blankCheck := strings.Map(runesRemover, data)
+	if blankCheck == "" {
+		return morse.ToText(data)
+	}
+	return morse.ToMorse(data)
+}
