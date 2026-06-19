0fe73585b2179704102b6de5e36276be68f28fc1
Matveev Denis <denchittos@gmail.com>
2026-06-19 07:50:45 +0300

    Подгоняю под тест

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -15,6 +15,7 @@ func RootHandler(w http.ResponseWriter, _ *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
 }
@@ -71,6 +72,7 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(data))
 }
